fd0abe012a5ad6bab5d41cff29963c21cc630167
Alex <sashalytko@mail.ru>
2018-12-20 00:11:51 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   let btn = document.querySelector('.burger-btn'),
-    menu = document.querySelector('.burger-menu');
+    menu = document.querySelector('.burger-menu'),
+    btnFooter = document.querySelector('.burger-btn__footer'),
+    menuFooter = document.querySelector('.burger-menu__footer');
 
   btn.addEventListener('click', function (e) {
     menu.classList.toggle('burger-menu_active');
   });
-})
-
-document.addEventListener('DOMContentLoaded', function () {
-
-  let btn = document.querySelector('.burger-btn__footer'),
-    menu = document.querySelector('.burger-menu__footer');
-
-  btn.addEventListener('click', function (e) {
-    menu.classList.toggle('burger-menu_active__footer');
+  btnFooter.addEventListener('click', function (e) {
+    menuFooter.classList.toggle('burger-menu_active__footer');
   });
 })
